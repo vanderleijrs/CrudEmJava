3a14145595b45425e30fe90496cdc1fcdd9b4378
vanderleijrs <vanderleijrs1977@gmail.com>
2020-01-04 15:01:42 -0300

    Criação da classe Cliente, e configuração do hibernate

---
+++ src/model/domain/Cliente.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author vanderlei
+ */
+@Entity
+@Table(name="TB_CLIENTE")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name="CD_CLIENTE")
+    private Integer codigo;
+    @Column(name="NM_ClIENTE",length = 255,nullable = false)
+    private String nome;
+    public Cliente(){
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
